17250a04a36572861939d4d35487b81e6f836371
Asterisk <kuro5858tube@gmail.com>
2017-05-20 23:52:02 +0900

    #3 Neptune Core Files Commit.
    Neptune Core Files Commit.

---
+++ Models/Twitter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.Linq;
+using System.Windows.Input;
+
+using CoreTweet;
+using static System.Console;
+
+
+namespace Neptune.Models
+{
+    /// <summary>
+    /// Twitter ; 認証～画像取得までのプロセスを援助します。
+    /// </summary>
+    public class Twitter
+    {
+        #region 認証パラメータ
+        private const int       ARY_SIZE = 4;
+        private static string[] Authorize;
+
+        private static string   consumer_key;
+        private static string   consumer_key_secret;
+        private static string   access_token;
+        private static string   access_token_secret;
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Twitter(){ }
+
+        /// <summary>
+        /// 初期化を行います。
+        /// </summary>
+        public void Initialize()
+        {
+            // 配列の初期化
+            Authorize       = new string[ ARY_SIZE] { null, null, null, null };
+                  
+            // app.config から情報取得
+            var appSettings = ConfigurationManager.AppSettings.AllKeys;
+
+            try
+            {
+                // app.config : key の要素分だけループ
+                for ( int keyElements = 0; keyElements < appSettings.Length; keyElements++ )
+                {
+                    // 各 key.value の値を配列に格納する
+                    Authorize[ keyElements] = ConfigurationManager.AppSettings[ keyElements];
+
+                    // 配列から各認証パラメータメンバに対して格納する
+                    switch ( keyElements )
+                    {
+                        case 0: consumer_key        = Authorize[ keyElements]; break;
+                        case 1: consumer_key_secret = Authorize[ keyElements]; break;
+                        case 2: access_token        = Authorize[ keyElements]; break;
+                        case 3: access_token_secret = Authorize[ keyElements]; break;
+                        default: throw new Exception();
+                    }
+                }
+            }
+            catch ( Exception ex )
+            {
+                WriteLine( ex.StackTrace );
+            }
+
+        }
+
+        /// <summary>
+        /// 現在再生している曲を Twitter に投稿します。
+        /// <param name=Track>現在再生している曲名。</param>
+        /// <param name=Artist>現在再生している曲のアーティスト名。</param>
+        /// </summary>
+        public bool Tweet()
+        {
+            // API にアクセスするためのトークン群
+            var tokens = Tokens.Create(
+                $"{consumer_key}",
+                $"{consumer_key_secret}",
+                $"{access_token}",
+                $"{access_token_secret}"
+            );
+
+            // Tweet 用に文字列を生成する
+            string Mix = "Neptune Twitter API Access Test.";
+
+            try
+            {
+                tokens.Statuses.Update(
+                    new
+                    {
+                        status = Mix
+                    }
+                );
+
+                Console.WriteLine($"{Mix}");
+            }
+
+            // ツイートが重複した際に catch する。
+            catch (Exception ex)
+            {
+                string msg = $"Neptune is Tweet Overlapped.";
+
+                Console.WriteLine(ex.Message);
+                tokens.Statuses.Update(new { status = msg });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+
+using Neptune.Models;
+using MahApps.Metro.Controls;
+
+namespace Neptune.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        // Twitter クラスインスタンス
+        private Twitter _twitter;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // IcePlayer のウィンドウ移動(ドラッグ) を可能にする
+            this.MouseLeftButtonDown += (sender, e) => this.DragMove();
+
+            _twitter = new Twitter();
+
+            _twitter.Initialize();
+            _twitter.Tweet();
+        }
+    }
+}
