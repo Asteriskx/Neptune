3811fdad69e559c491a56cfc0448b2b61a5e2279
Asterisk <kuro5858tube@gmail.com>
2017-05-21 22:45:19 +0900

    #5 Added Enum Files.
    Added ImageSize.cs

---
+++ Enum/ImageSize.cs
@@ -0,0 +1,23 @@
+﻿namespace Neptune.Enum
+{
+    /// <summary>
+    /// image のサイズの定義を行います。
+    /// </summary>
+    public enum ImageSize
+    {
+        Minimum = 0,       
+        Normal,    
+        Bigger    
+    };
+
+    /// <summary>
+    /// 認証パラメータ設定時の 配列指定に使用します。
+    /// </summary>
+    public enum SwitchType
+    {
+        CONSUMER_KEY = 0,
+        CONSUMER_KEY_S,
+        ACCESS_TOKEN,
+        ACCESS_TOKEN_S
+    }
+}
